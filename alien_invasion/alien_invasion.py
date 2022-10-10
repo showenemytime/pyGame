0ffb64ffb83592910fef3ae596637a09c9aadef0
aleksandrzdanov <zhdanov@magora-systems.com>
2022-10-10 10:26:15 +0500

    first commit

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,39 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+   #  Класс для поведения ресурсами и поведением игры
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption('Alien Invasion')
+
+        self.ship = Ship(self)
+
+        self.bg_color = self.settings.bg_color
+
+    def run_game(self):
+        #  Запуск основного цикла игры.
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.bg_color)
+            self.ship.blitme()
+
+            # Отображение последнего прорисованного экрана.
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    #  Создание экзамляра и запуск
+    ai = AlienInvasion()
+    ai.run_game()
+
+
