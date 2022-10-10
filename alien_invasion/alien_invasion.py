d8c1273ca016b1ab1cce37f054e97c7cf01994e9
aleksandrzdanov <zhdanov@magora-systems.com>
2022-10-10 23:00:33 +0500

    day 2. add new func.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -16,20 +16,29 @@ class AlienInvasion:
 
         self.bg_color = self.settings.bg_color
 
+
     def run_game(self):
         #  Запуск основного цикла игры.
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.bg_color)
-            self.ship.blitme()
 
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
 
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # При каждом проходе цикла перерисовывается экран.
+
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #  Создание экзамляра и запуск
